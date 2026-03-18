120498abfd2dc2d67df3642e6c4edd7a8355ef72
soumil-kumar <hellosoumil@gmail.com>
2026-03-18 17:51:55 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,9 @@ json decode_bencoded_value(const std::string& encoded_value) {
         } else {
             throw std::runtime_error("Invalid encoded value: " + encoded_value);
         }
-    } else {
+    }else if(encoded_value[0] == 'i' && encoded_value.back() == 'e'){
+        return (encoded_value.substr(1, encoded_value.size()-2));
+    }else {
         throw std::runtime_error("Unhandled encoded value: " + encoded_value);
     }
 }   
