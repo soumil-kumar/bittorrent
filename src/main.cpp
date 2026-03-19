510c7570704194a46aa12e8c5d075f3ecbf0d55c
soumil-kumar <hellosoumil@gmail.com>
2026-03-19 20:00:20 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -8,22 +8,24 @@
 
 using json = nlohmann::json;
 
-json decode_bencoded_value(const std::string& encoded_value, int &offset) {
+int default_offset;
+
+json decode_bencoded_value(const std::string& encoded_value, int *offset = &default_offset) {
     if (std::isdigit(encoded_value[0])) {
         // Example: "5:hello" -> "hello"
         size_t colon_index = encoded_value.find(':');
         if (colon_index != std::string::npos) {
             std::string number_string = encoded_value.substr(0, colon_index);
             int64_t number = std::atoll(number_string.c_str());
-            offset = colon_index + 1 + number;
+            *offset = colon_index + 1 + number;
             std::string str = encoded_value.substr(colon_index + 1, number);
             return json(str);
         } else {
             throw std::runtime_error("Invalid encoded value: " + encoded_value);
         }
     }else if (encoded_value[0] == 'i') {
-        offset = encoded_value.find('e') + 1;
-        return json(stol(encoded_value.substr(1, offset)));
+        *offset = encoded_value.find('e') + 1;
+        return json(stol(encoded_value.substr(1, *offset)));
     }else if (encoded_value[0] == 'l') {
         auto res = json::array({});
         int beg = 1;
@@ -31,12 +33,12 @@ json decode_bencoded_value(const std::string& encoded_value, int &offset) {
         while(beg+1 < encoded_value.length()) {
             if(encoded_value[beg] == 'e') break;
             auto str = encoded_value.substr(beg);
-            auto e = decode_bencoded_value(str, end);
+            auto e = decode_bencoded_value(str, &end);
             //if(end == -1) break;
             res.push_back(e);
             beg += end;
         }
-        offset = beg + 1;
+        *offset = beg + 1;
         return res;
     }else {
         throw std::runtime_error("Unhandled encoded value: " + encoded_value);
@@ -47,7 +49,7 @@ int main(int argc, char* argv[]) {
     // Flush after every std::cout / std::cerr
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
-
+    std::cout<<default_offset<<'\n';
     if (argc < 2) {
         std::cerr << "Usage: " << argv[0] << " decode <encoded_value>" << std::endl;
         return 1;
