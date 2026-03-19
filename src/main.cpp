1275ba4158592b6451861b2d416a383c19e01101
soumil-kumar <hellosoumil@gmail.com>
2026-03-19 20:10:55 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -8,24 +8,22 @@
 
 using json = nlohmann::json;
 
-int default_offset;
-
-json decode_bencoded_value(const std::string& encoded_value, int *offset = &default_offset) {
+json decode_bencoded_value(const std::string& encoded_value, int &offset) {
     if (std::isdigit(encoded_value[0])) {
         // Example: "5:hello" -> "hello"
         size_t colon_index = encoded_value.find(':');
         if (colon_index != std::string::npos) {
             std::string number_string = encoded_value.substr(0, colon_index);
             int64_t number = std::atoll(number_string.c_str());
-            *offset = colon_index + 1 + number;
+            offset = colon_index + 1 + number;
             std::string str = encoded_value.substr(colon_index + 1, number);
             return json(str);
         } else {
             throw std::runtime_error("Invalid encoded value: " + encoded_value);
         }
     }else if (encoded_value[0] == 'i') {
-        *offset = encoded_value.find('e') + 1;
-        return json(stol(encoded_value.substr(1, *offset)));
+        offset = encoded_value.find('e') + 1;
+        return json(stol(encoded_value.substr(1, offset)));
     }else if (encoded_value[0] == 'l') {
         auto res = json::array({});
         int beg = 1;
@@ -33,12 +31,12 @@ json decode_bencoded_value(const std::string& encoded_value, int *offset = &defa
         while(beg+1 < encoded_value.length()) {
             if(encoded_value[beg] == 'e') break;
             auto str = encoded_value.substr(beg);
-            auto e = decode_bencoded_value(str, &end);
+            auto e = decode_bencoded_value(str, end);
             //if(end == -1) break;
             res.push_back(e);
             beg += end;
         }
-        *offset = beg + 1;
+        offset = beg + 1;
         return res;
     }else {
         throw std::runtime_error("Unhandled encoded value: " + encoded_value);
@@ -49,7 +47,6 @@ int main(int argc, char* argv[]) {
     // Flush after every std::cout / std::cerr
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
-    std::cout<<default_offset<<'\n';
     if (argc < 2) {
         std::cerr << "Usage: " << argv[0] << " decode <encoded_value>" << std::endl;
         return 1;
@@ -64,10 +61,10 @@ int main(int argc, char* argv[]) {
         }
         // You can use print statements as follows for debugging, they'll be visible when running tests.
         std::cerr << "Logs from your program will appear here!" << std::endl;
-
+        int offset = 0;
         // TODO: Uncomment the code below to pass the first stage
         std::string encoded_value = argv[2];
-        json decoded_value = decode_bencoded_value(encoded_value);
+        json decoded_value = decode_bencoded_value(encoded_value, offset);
         std::cout << decoded_value.dump() << std::endl;
     } else {
         std::cerr << "unknown command: " << command << std::endl;
